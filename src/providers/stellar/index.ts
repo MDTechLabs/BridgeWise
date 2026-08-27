8c598f00ed8bf10824444d0492eae0e812f4beed
Gyanee01 <gyanendra2301@gmail.com>
2026-08-27 10:32:56 +0530

    feat: add Stellar bridge provider adapter interface

--- src/providers/stellar/index.ts
+++ src/providers/stellar/index.ts
@@ -1,4 +1,8 @@
 export * from './history';
 export * from './score';
 export * from './flags';
+<<<<<<< HEAD
 export * from './settlement';
+=======
+export * from './adapters';
+>>>>>>> c1555ef2 (feat: add Stellar bridge provider adapter interface)
